ede0fea221d670222a8b8e14800e24bb4df1b135
Cesar Martinez <cmmh200427@gmail.com>
2024-08-12 01:15:14 -0400

    animacion pop añadida a modal

--- pokemania/src/components/pokedex/FilterModal.tsx
+++ pokemania/src/components/pokedex/FilterModal.tsx
@@ -1,13 +1,18 @@
-import { useContext, useState } from 'react';
-import { ModalProps } from '../../interfaces/ModalProps';
-import { Modal } from '../ui/Modal';
-import { regionsData } from '../../helpers/region';
-import { typesData } from '../../helpers/types';
-import { PokemonContext } from '../../context/PokedexContext';
+import { useContext, useState } from "react";
+import { ModalProps } from "../../interfaces/ModalProps";
+import { Modal } from "../ui/Modal";
+import { regionsData } from "../../helpers/region";
+import { typesData } from "../../helpers/types";
+import { PokemonContext } from "../../context/PokedexContext";
 
-export const FilterModal: React.FC<ModalProps> = ({isOpen, setIsOpen, setActiveIndex, setTranslate}) => {
-
-  const { setRegion, setType, setPage, setFetchByRegion } = useContext(PokemonContext)
+export const FilterModal: React.FC<ModalProps> = ({
+  isOpen,
+  setIsOpen,
+  setActiveIndex,
+  setTranslate,
+}) => {
+  const { setRegion, setType, setPage, setFetchByRegion } =
+    useContext(PokemonContext);
   const [selectedRegion, setSelectedRegion] = useState<number | null>(null);
   const [selectedTypes, setSelectedTypes] = useState<number | null>(null);
 
@@ -22,38 +27,52 @@ export const FilterModal: React.FC<ModalProps> = ({isOpen, setIsOpen, setActiveI
   };
 
   const handleFilter = () => {
-    if (selectedRegion!== null) {
-      setFetchByRegion(true)
+    if (selectedRegion !== null) {
+      setFetchByRegion(true);
       setRegion(selectedRegion);
-    } 
-    else if(selectedTypes !== null) {
-      setFetchByRegion(false)
+    } else if (selectedTypes !== null) {
+      setFetchByRegion(false);
       setType(selectedTypes);
     }
     setPage(1);
-    setIsOpen(false);  
-    setActiveIndex?.(0)
+    setIsOpen(false);
+    setActiveIndex?.(0);
     setTranslate?.(0);
-    };
-
+  };
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <h2 className='text-2xl'>Filtros</h2>
-      <button className='btn sm:w-[20%] w-1/2 py-1 ml-auto rounded-md mb-2' onClick={handleFilter}>Filtrar</button>
-      <article className='grid gap-10 grid-cols-2 '>
+      <h2 className="text-2xl">Filtros</h2>
+      <button
+        className="btn sm:w-[20%] w-1/2 py-1 ml-auto rounded-md mb-2"
+        onClick={handleFilter}
+      >
+        Filtrar
+      </button>
+      <article className="grid gap-10 grid-cols-2 ">
         <div>
-          <h3 className='text-lg mb-3'>Regiones</h3>
-          <ul className='grid md:grid-cols-2 gap-2'>
+          <h3 className="text-lg mb-3">Regiones</h3>
+          <ul className="grid md:grid-cols-2 gap-2">
             {regionsData.map((region) => (
-              <li key={region.id} className='flex gap-4 rounded-md'>
+              <li key={region.id} className="flex gap-4 rounded-md">
                 <button
                   className={`flex gap-1 justify-center py-2 font-pokedex 
                             text-bgWhite w-full rounded btn
-                            ${selectedRegion === region.id? 'bg-bgRed' : 'bg-bgPokedex'}`}
+                            ${
+                              selectedRegion === region.id
+                                ? "bg-bgRed"
+                                : "bg-bgPokedex"
+                            }`}
                   onClick={() => handleRegionSelect(region.id)}
                 >
-                  {region.name} <span className='hidden md:block'> - {region.generation === null? "All" : region.generation} Gen</span> 
+                  {region.name}{" "}
+                  <span className="hidden md:block">
+                    {" "}
+                    - {region.generation === null
+                      ? "All"
+                      : region.generation}{" "}
+                    Gen
+                  </span>
                 </button>
               </li>
             ))}
@@ -61,27 +80,35 @@ export const FilterModal: React.FC<ModalProps> = ({isOpen, setIsOpen, setActiveI
         </div>
 
         <div>
-          <h3 className='text-lg mb-3'>Tipos</h3>
-            <ul className='grid grid-cols-2 sm:grid-cols-3 gap-2'>
+          <h3 className="text-lg mb-3">Tipos</h3>
+          <ul className="grid grid-cols-2 sm:grid-cols-3 gap-2">
             {typesData.map((type) => (
-                <li key={type.id} className={`flex justify-center md:justify-start gap-2 p-2 rounded-full sm:rounded-md ${selectedTypes === type.id ? 'opacity-100 border-bgBlack border-solid border-2' : 'opacity-50'}`}
-                    style={{ backgroundColor: type.color }}
-                    onClick={() => handleTypeSelect(type.id)}
-                    >
-                    <input
-                    className='hidden'
-                    type="checkbox"
-                    name="type"
-                    value={type.type}
-                    onChange={() => setSelectedTypes(type.id)}
-                    />
-                    <label className='flex gap-2'>
-                        <img src={type.img} alt={type.type} className="w-6 h-6" />
-                        <span className='font-pokedex text-bgWhite capitalize hidden md:block'>{type.type}</span>
-                    </label>
-                </li>
-                ))}
-            </ul>
+              <li
+                key={type.id}
+                className={`flex justify-center md:justify-start gap-2 p-2 rounded-full sm:rounded-md ${
+                  selectedTypes === type.id
+                    ? "opacity-100 border-bgBlack border-solid border-2"
+                    : "opacity-50"
+                }`}
+                style={{ backgroundColor: type.color }}
+                onClick={() => handleTypeSelect(type.id)}
+              >
+                <input
+                  className="hidden"
+                  type="checkbox"
+                  name="type"
+                  value={type.type}
+                  onChange={() => setSelectedTypes(type.id)}
+                />
+                <label className="flex gap-2">
+                  <img src={type.img} alt={type.type} className="w-6 h-6" />
+                  <span className="font-pokedex text-bgWhite capitalize hidden md:block">
+                    {type.type}
+                  </span>
+                </label>
+              </li>
+            ))}
+          </ul>
         </div>
       </article>
     </Modal>
--- pokemania/src/components/ui/Modal.tsx
+++ pokemania/src/components/ui/Modal.tsx
@@ -1,48 +1,55 @@
-import { ElementRef, useCallback, useEffect, useRef } from "react";
+import { ElementRef, useEffect, useRef } from "react";
 import { ModalProps } from "../../interfaces/ModalProps";
 
 export const Modal = ({ children, isOpen, setIsOpen }: ModalProps) => {
   const dialogRef = useRef<ElementRef<"dialog">>(null);
 
-  const closeModalWithKeyboard = useCallback(
-    (e: KeyboardEvent) => {
+  useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal();
+      document.body.style.overflowY = "hidden";
+    } else {
+      const timeout = setTimeout(() => {
+        dialogRef.current?.close();
+        document.body.style.overflowY = "auto";
+      }, 300);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    const closeModalWithKeyboard = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
         e.preventDefault();
         setIsOpen(false);
       }
-    },
-    [setIsOpen]
-  );
-  useEffect(() => {
-    if (isOpen) {
-      dialogRef.current?.showModal();
-      document.addEventListener("keydown", closeModalWithKeyboard);
-      document.body.style.overflowY = "hidden";
-    } else {
-      dialogRef.current?.close();
+    };
+
+    document.addEventListener("keydown", closeModalWithKeyboard);
+    return () => {
       document.removeEventListener("keydown", closeModalWithKeyboard);
-      document.body.style.overflowY = "auto";
-    }
-  }, [isOpen, closeModalWithKeyboard]);
+    };
+  }, [setIsOpen]);
+
   return (
-    <>
-      <dialog
-        className="w-3/4 transition-all p-8 border-3 
-			border-bgGrey border-solid rounded-lg fixed bottom-36 md:bottom-0"
-        ref={dialogRef}
-      >
-        <div className="absolute top-4 right-4 flex items-center gap-2">
-          <span className="text-bgBlack opacity-75">Esc |</span>
-          <button
-            className="transition-all bg-bgGray px-2 rounded-md opacity-60 border border-bgBlack border-solid hover:opacity-100"
-            autoFocus
-            onClick={() => setIsOpen(false)}
-          >
-            x
-          </button>
-        </div>
-        <div className="modal__content">{children}</div>
-      </dialog>
-    </>
+    <dialog
+      className={`w-3/4 transition-all p-8 border-3 
+      border-bgGrey border-solid rounded-lg fixed bottom-36 md:bottom-0
+      ${isOpen ? "animate-fadeIn" : "animate-fadeOut"}`}
+      ref={dialogRef}
+    >
+      <div className="absolute top-4 right-4 flex items-center gap-2">
+        <span className="text-bgBlack opacity-75">Esc |</span>
+        <button
+          className="transition-all bg-bgGray px-2 rounded-md opacity-60 border border-bgBlack border-solid hover:opacity-100"
+          autoFocus
+          onClick={() => setIsOpen(false)}
+        >
+          x
+        </button>
+      </div>
+      <div className="modal__content">{children}</div>
+    </dialog>
   );
 };
